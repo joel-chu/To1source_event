2acc9999f5b6215b457b41eb8126ea57c05b55a6
Joel Chu <joelckchu@gmail.com>
2023-02-13 15:06:46 +0800

    prepare ts env

---
+++ src/lib/utils.ts
@@ -0,0 +1,74 @@
+
+/*
+ * More accurately check the type of a JavaScript object
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ */
+export function trueTypeOf (obj: unknown): string {
+	return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+}
+
+/**
+ * generate a 32bit hash based on the function.toString()
+ * _from http://stackoverflow.com/questions/7616461/generate-a-hash-_from-string-in-javascript-jquery
+ */
+export function hashCode (s: string): string {
+  // @ts-ignore @TODO convert the string to number
+  return s.split('')
+    .reduce(function (a, b) {
+      a = ((a << 5) - a) + b.charCodeAt(0);
+      return a & a
+    }, 0)
+}
+
+/**
+ * wrapper to make sure it string
+ */
+export function hashCode2Str (s: string): string {
+  return hashCode(s) + ''
+}
+
+/**
+ * Just check if a pattern is an RegExp object
+ */
+export function isRegExp (pat: unknown): boolean {
+  return pat instanceof RegExp
+}
+
+/**
+ * check if its string
+ */
+export function isString (arg: unknown): boolean {
+  return trueTypeOf(arg) === 'string'
+}
+
+/**
+ * check if it's an integer
+ */
+export function isInt (num: unknown): boolean {
+  if (isString(num)) {
+    throw new Error('Wrong type, we want number!')
+  }
+  return !isNaN(parseInt(num as string))
+}
+
+/**
+ * Find from the array by matching the pattern
+ */
+export function getRegex (pattern: unknown): boolean | RegExp {
+  switch (true) {
+    case isRegExp(pattern) === true:
+      return pattern as RegExp
+    case isString(pattern) === true:
+      return new RegExp(pattern as string)
+    default:
+      return false
+  }
+}
+
+/**
+ * in array
+ */
+export const inArray = (
+  arr: Array<unknown>,
+  prop: unknown
+) => arr.includes(prop) // 2022 version
---
+++ src/lib/watch.ts
@@ -0,0 +1,40 @@
+// a polyfill to create global watch method
+// from https://abdulapopoola.com/2015/04/17/how-to-watch-variables-in-javascript/
+// without own modification to make it NOT polluting the global prototype chain
+class WatchClass {}
+export declare type WatchProps = any; // @TODO stub it for now
+export declare type CallbackHandler = (...args: any[]) => void
+// @TODO upgrade it with Proxy
+if (!WatchClass.prototype.watch) {
+  Object.defineProperty(WatchClass.prototype, 'watch', {
+    enumerable: false,
+    configurable: true,
+    writable: false,
+    value: function (prop: WatchProps, handler: CallbackHandler) {
+      var old = this[prop]
+      var cur = old
+      var getter = function () {
+        return cur
+      }
+      var setter = function (val: unknown) {
+        old = cur
+        // We change the order of the params
+        cur = handler.call(this, val, prop, old)
+
+        return cur
+      }
+      // can't watch constants
+      if (delete this[prop]) {
+        Object.defineProperty(this, prop, {
+          get: getter,
+          set: setter,
+          enumerable: true,
+          configurable: true
+        })
+      }
+    }
+  })
+}
+
+// instead of polluting the global prototype we create this as an class method instead
+export { WatchClass }
