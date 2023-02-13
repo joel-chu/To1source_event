28f25b7606af9a10c0a7e13eeb712c6b6fd65bd2
Joel Chu <joelckchu@gmail.com>
2023-02-13 15:06:46 +0800

    prepare ts env

---
+++ src/base.ts
@@ -0,0 +1,14 @@
+// setup a base class to put all the share methods
+import { hashCode2Str, isString } from './lib/utils'
+import { AVAILABLE_TYPES } from './lib/constants'
+
+export declare type EventClassConfig = any // @TODO
+
+
+export default class BaseClass {
+
+  constructor(config: EventClassConfig = {}) {
+    
+  }
+
+}
---
+++ src/lib/stores.ts
@@ -0,0 +1,3 @@
+// Create two WeakMap store as a private keys
+export const NB_EVENT_SERVICE_PRIVATE_STORE = new WeakMap()
+export const NB_EVENT_SERVICE_PRIVATE_LAZY = new WeakMap()
