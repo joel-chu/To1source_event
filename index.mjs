ce3f8b369ed6f2204b0b6b35c5a8f5bc0a8da20b
Joel Chu <joelckchu@gmail.com>
2023-02-27 11:10:55 +0800

    move alias into src and prepare for new export structure

--- index.mjs
+++
@@ -1,4 +0,0 @@
-// default
-import To1sourceEvent from './src/event-service.mjs'
-
-export default To1sourceEvent
--- src/alias.ts
+++ src/alias.ts
@@ -1,42 +1,45 @@
 // this is an alias class that will create several alias method to their counter part
 // that will match up the other EventEmitter library out there for the developer to swap over
+// this will also get build into the dist folder, only we are only use the export property to
+// map different output
 
-import EventService from './src/event-service.mjs'
+import { EventClass } from './event'
 
-export default class AliasEventService extends EventService {
+export default class AliasEventClass extends EventClass {
+  
   constructor (options = {}) {
     super(options)
   }
 
-  on (...args) {
+  on (...args: Array<unknown>) {
     return Reflect.apply(this.$on, this, args)
   }
 
-  off (...args) {
+  off (...args: Array<unknown>) {
     return Reflect.apply(this.$off, this, args)
   }
 
-  emit (...args) {
+  emit (...args: Array<unknown>) {
     return Reflect.apply(this.$trigger, this, args)
   }
 
-  once (...args) {
+  once (...args: Array<unknown>) {
     return Reflect.apply(this.$once, this, args)
   }
 
-  only (...args) {
+  only (...args: Array<unknown>) {
     return Reflect.apply(this.$only, this, args)
   }
 
-  onlyOnce (...args) {
+  onlyOnce (...args: Array<unknown>) {
     return Reflect.apply(this.$onlyOnce, this, args)
   }
 
-  get (...args) {
+  get (...args: Array<unknown>) {
     return Reflect.apply(this.$get, this, args)
   }
 
-  replace (...args) {
+  replace (...args: Array<unknown>) {
     return Reflect.apply(this.$replace, this, args)
   }
 }
